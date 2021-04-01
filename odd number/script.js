5f2f14dcbbeec3eb8f3f30f322ad6797af7a6695
veluvijayan <81183634+veluvijayan@users.noreply.github.com>
2021-04-01 00:35:03 -0700

    Update script.js

--- odd number/script.js
+++ odd number/script.js
@@ -1,6 +1,7 @@
-var arr=[1,2,3,4];
-for(var i=0;i<=arr.length;i++){
-    if(i %2!==0){
-        console.log(i);
-    }
-}
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+let odds = arr.filter((item)=>{
+    return item%2!==0;
+});
+
+console.log(odds);
